dfeaa10776cc644f202337ade8ef26f322585b6c
dea-alihajdari <116096272+dea-alihajdari@users.noreply.github.com>
2025-03-11 19:03:45 +0100

    dea

---
+++ medium/main.js
@@ -0,0 +1,19 @@
+
+    console.log("Starting in 3 seconds...");
+
+
+    setTimeout(function() {
+      console.log("3...");
+    }, 1000);  
+
+    setTimeout(function() {
+      console.log("2...");
+    }, 2000);  
+
+    setTimeout(function() {
+      console.log("1...");
+    }, 3000);  
+
+    setTimeout(function() {
+      console.log("Go!");
+    }, 4000);  
